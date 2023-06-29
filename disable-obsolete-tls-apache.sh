eeae3ea335b2a35f0528facf166b653e83d929e0
rehp-sec <137841209+rehp-sec@users.noreply.github.com>
2023-06-28 21:20:50 -0400

    Create disable-obsolete-tls-apache.sh

---
+++ disable-obsolete-tls-apache.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+# Buscar el archivo de configuración
+config_file=$(find / -name "ssl.conf" -o -name "httpd-ssl.conf" 2>/dev/null)
+
+# Verificar si se encontró el archivo
+if [ -z "$config_file" ]; then
+  echo "No se pudo encontrar el archivo de configuración."
+  exit 1
+fi
+
+# Editar el archivo de configuración
+sed -i 's/^\(SSLProtocol .*\)$/SSLProtocol all -SSLv3 -TLSv1 -TLSv1.1/' "$config_file"
+
+# Reiniciar el servicio de Apache2
+service apache2 restart
